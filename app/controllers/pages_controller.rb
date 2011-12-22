f6d0a16b02a0070c268ed30d275015e6c9fa5137
Fernando Lamas <lamas.fernando1419@gmail.com>
2011-12-22 20:45:46 -0300

    Adding some dinamyc contents

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,12 @@
 class PagesController < ApplicationController
-  def home
-    # home page
+  def home # home page 
+    # this instance variable @title will be available in the home.html.erb view    
   end
 
-  def contact
-    # contact page
+  def contact  # contact page    
   end
 
-  def about 
-    # describes the about action
+  def about # describes the about action    
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,12 +8,24 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get :home
+      # have_selector(checks the html element (title) with its correpondent content
+      # (Medical Stuff..)) inside of it, it also matches a partial content.
+      response.should have_selector('title', :content => 'Medical Stuff - Home')
+    end
   end
 
   describe "GET 'contact'" do # when visite contact page it should return 200 code (success)
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get :contact
+      response.should have_selector('title', :content => 'Medical Stuff - Contact')
     end
   end  
 
@@ -22,6 +34,10 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-  end
 
+    it "should have the right title" do
+      get :about
+      response.should have_selector('title', :content => 'Medical Stuff - About')
+    end
+  end
 end
