29158003f99b3d0eb60fe0be0b716ce69f6803a9
Fernando Lamas <lamas.fernando1419@gmail.com>
2012-01-03 20:54:24 -0300

    Adding more tests to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,12 @@ class User < ActiveRecord::Base
 #  created_at :datetime
 #  updated_at :datetime
 #
-attr_accessible :name, :email
-  validates :name, :presence => true  # validates presence of name attribute  
+  attr_accessible :name, :email
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :name, :presence => true, 
+                   :length => {:minimum => 5, :maximum => 50}
+  validates :email, :presence => true, 
+                    :format => { :with => email_regex },
+                    :uniqueness => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,26 +15,58 @@ describe User do
     @attr = {:name => 'Juan Carlos', :email => 'car@car.com'}       
   end
 
-  it "should create a new instance given valid attributes" do
+  it "should create a new instance given valid attributes" do    
     lambda {
       User.create(@attr)   
     }.should change(User, :count).by(1)    
   end
 
   it "should be invalid with an empty name" do        
-    a = User.new(@attr.merge(:name => ""))    
-    a.valid?.should == false
-    a.should have(1).errors_on(:name)    
+    user_without_name = User.new(@attr.merge(:name => ""))    
+    user_without_name.valid?.should == false
+    # user_without_name.should have(1).errors_on(:name)    
   end
 
-  # it "should be invalid with an empty email"
+  it "should be invalid with an empty email" do
+    user_without_email = User.new(@attr.merge(:email => ""))
+    user_without_email.valid?.should == false
+    # user_without_email.should have(1).errors_on(:email)
+  end
 
-  # it "should reject names that are too long"
+  it "should be invalid with names with more than 50 characters long" do
+    long_name = 'a' * 51
+    user_with_too_long_name = User.new(@attr.merge(:name => long_name))
+    user_with_too_long_name.valid?.should == false
+  end
 
-  # it "should accept valid email addresses"
+  it "should be invalid with names with less than 5 characters long" do
+    short_name = 'a' * 4
+    user_with_too_short_name = User.new(@attr.merge(:name => short_name))
+    user_with_too_short_name.valid?.should == false
+  end
+
+  it "should accept valid email addresses" do
+    valid_emails = %w[user@email.com user.surname@gmail.com user_1@gmail.com.ar]
+    valid_emails.each do |email|
+      valid_email_user = User.new(@attr.merge(:email => email))
+      valid_email_user.valid?.should == true
+    end
+  end
      
-  # it "should reject invalid email addresses"
+  it "should reject invalid email addresses" do
+    invalid_emails = %w[user#email.com user.surname@.com user,1@gmail.com.ar]
+    invalid_emails.each do |email|
+      invalid_email_user = User.new(@attr.merge(:email => email))
+      invalid_email_user.valid?.should == false
+    end
+  end
 
-  # it "should reject duplicate email addresses"
+  ## Uniqueness Validation
+  it "should reject duplicate email addresses" do
+    # records a user with given email address into the DB.
+    User.create!(@attr) # one user with an email address
+    user_with_duplicate_email = User.new(@attr) # another user with same email 
+    user_with_duplicate_email.valid?.should == false
+  end
 
 end
