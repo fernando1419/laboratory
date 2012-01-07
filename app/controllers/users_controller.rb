6114ccc986c5cf98a07b79b7eaa901a3cc84c5e2
Fernando Lamas <lamas.fernando1419@gmail.com>
2012-01-06 21:03:56 -0300

    Beginning with signing-up users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new # because this the form method in the view is post
     @title = 'Sign up'
   end
 
@@ -8,4 +9,15 @@ class UsersController < ApplicationController
     @title = @user.name
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save # (true or false)
+      # handle a successful save.
+    else 
+      # handle a save failure (describe failure testing)
+      @title = 'Sign up' 
+      render :action => 'new'
+    end
+  end
+ 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -37,4 +37,27 @@ render_views
     end
   end
 
+  describe "POST 'create'" do
+    describe "failure" do # tests for failed user signup      
+      before(:each) do
+        @attr = {:name => "", :email => "", :password => "",
+                 :password_confirmation => ""}
+      end
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector(:title, :content => 'Sign up')
+      end
+      # tries to create a new user with invalid fields
+      it "should not create a new user" do
+        lambda do 
+          post :create, :user => @attr  # post request with invalid user attrs
+        end.should_not change(User, :count)
+      end
+      # when fails it renders the new template
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template(:new)
+      end
+    end #describe failure
+  end #describe POST 'create'
 end
